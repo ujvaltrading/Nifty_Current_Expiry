7ef9e90d0cfd0902e2ffb2aebf75569ce8206317
ujvaltrading <ujvaltrading@gmail.com>
2025-04-23 03:12:18 +0530

    Delete symbolMap.js

--- symbolMap.js
+++
@@ -1,6 +0,0 @@
-const symbolMap = [
-  { name: "Nifty 50", symbol: "NIFTY" },
-  { name: "Bank Nifty", symbol: "BANKNIFTY" },
-  { name: "Tata Consultancy Services Ltd", symbol: "TCS" },
-  { name: "Reliance Industries Ltd", symbol: "RELIANCE" }
-];
