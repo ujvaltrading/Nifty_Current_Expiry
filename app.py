3658e1ad0b008c1f7ac5fbb5e5b99bdd7d108f53
ujvaltrading <ujvaltrading@gmail.com>
2025-04-23 11:42:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,12 @@ def option_chain():
             })
 
         return jsonify(response)
+        import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
